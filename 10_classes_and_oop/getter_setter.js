ee7393cb2e83b484b44e0e1b90525902783891e1
ritwikranjan72 <dazzerstat@gmail.com>
2025-09-03 04:30:38 +0000

    save it

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        return this._email = value.toUpperCase()
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+
+    set password(value){
+        this._password = value.toUpperCase()
+    }
+}
+
+const ritwik = new User("r@ritwik.ai", "123")
+console.log(ritwik.email);
